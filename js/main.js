21c6fb17683ba0a243bcec5dd33a04517823cacc
KaterinaProkopenko <ekaprokopenko97@gmail.com>
2022-03-10 17:41:48 +0100

    Aggiornamento (main img)

--- js/main.js
+++ js/main.js
@@ -16,3 +16,84 @@ E se volessi un bottone per invertire la "direzione" del carosello?
 
  *
  */
+
+const animals = [
+    {
+        animal: 'cat',
+        image: 'cat.jpg',
+    },
+
+    {
+        animal: 'parrots',
+        image: 'parrots.jpg'
+    },
+
+    {
+        animal: 'fish',
+        image: 'fish.jpg'
+    },
+
+    {
+        animal: 'alpacas',
+        image: 'alpacas.jpg'
+    },
+
+    {
+        animal: 'eagle',
+        image: 'eagle.jpg'
+    },
+];
+
+let mainCarouselImages = [];
+
+for (let i = 0 ; i < animals.length ; i++){
+    console.log(animals[i].image);
+    const imageOfAnimal = document.createElement('img');
+    imageOfAnimal.src = `img/${animals[i].image}`;
+    mainCarouselImages.push(imageOfAnimal);
+    
+        /* mainCarouselImages.push(`<img src="img/${animals[i].image}" class="active" alt="Picture of an animal">`); */
+};
+
+console.log(mainCarouselImages);
+
+let mainCarouselContainer = document.querySelector('.my-carousel-images');
+
+for(let i = 0; i < mainCarouselImages.length; i++) {
+    console.log(mainCarouselImages[i]);
+    mainCarouselContainer.appendChild(mainCarouselImages[i]);
+}
+
+mainCarouselImages[0].classList.add('active');
+
+let activeElement = 0;
+
+const buttonNext = document.querySelector('div.my-next');
+
+buttonNext.addEventListener('click', function(){
+    mainCarouselImages[activeElement].classList.remove('active');
+
+    if (activeElement === mainCarouselImages.length - 1){
+        activeElement = 0;
+    } else {
+        activeElement++;
+    }
+
+    mainCarouselImages[activeElement].classList.add('active');
+});
+
+const buttonPrevious = document.querySelector('div.my-previous');
+
+buttonPrevious.addEventListener('click', function(){
+    mainCarouselImages[activeElement].classList.remove('active');
+
+    if (activeElement === 0){
+        activeElement = mainCarouselImages.length - 1;
+    } else {
+        activeElement--;
+    }
+
+    mainCarouselImages[activeElement].classList.add('active');
+})
+
+
