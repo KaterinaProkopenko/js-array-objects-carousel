e647795e2d4b32fec8a5d58681a5be15d885c48f
KaterinaProkopenko <ekaprokopenko97@gmail.com>
2022-03-15 16:54:46 +0100

    Aggiornamento (title)

--- js/main.js
+++ js/main.js
@@ -1,22 +1,3 @@
-/**
- *
- *
- *
-Riprendiamo l'esercizio carosello e rifacciamolo, questa volta usando gli oggetti, prendendo come riferimento il codice scritto oggi insieme a lezione, che troverete direttamente nella mia repository di github a questo link: [link github]
-
-Modifichiamo il codice dell'esercizio per renderlo funzionante con un array di oggetti al posto dei tre array separati, con una sola regola: non è possibile modificare l'HTML ma solamente JS e CSS.
-Ricordiamo sempre l'importanza dell'integrità del dato.
-
-Bonus 1:
-Sperimentiamo attraverso l'uso delle timing functions anche una funzionalità di scorrimento al nostro carosello:
-al click di un bottone o già dall'inizio possiamo far partire, ad intervalli di tempo a piacere, lo scorrimento delle immagini disponibili nel carosello stesso.
-
-Bonus 2:
-E se volessi un bottone per invertire la "direzione" del carosello?
-
- *
- */
-
 const animals = [
     {
         animal: 'cat',
@@ -42,58 +23,108 @@ const animals = [
         animal: 'eagle',
         image: 'eagle.jpg'
     },
-];
+]; 
 
-let mainCarouselImages = [];
+const mainCarouselImages = [];
+const smallCarouselImages = [];
+const typeOfAnimal = [];
 
-for (let i = 0 ; i < animals.length ; i++){
-    console.log(animals[i].image);
-    const imageOfAnimal = document.createElement('img');
-    imageOfAnimal.src = `img/${animals[i].image}`;
-    mainCarouselImages.push(imageOfAnimal);
-    
-        /* mainCarouselImages.push(`<img src="img/${animals[i].image}" class="active" alt="Picture of an animal">`); */
-};
-
-console.log(mainCarouselImages);
-
-let mainCarouselContainer = document.querySelector('.my-carousel-images');
-
-for(let i = 0; i < mainCarouselImages.length; i++) {
-    console.log(mainCarouselImages[i]);
-    mainCarouselContainer.appendChild(mainCarouselImages[i]);
-}
-
-mainCarouselImages[0].classList.add('active');
+createImg (mainCarouselImages, '.my-carousel-images', 'active');
+createImg (smallCarouselImages, '.my-thumbnails', 'small-active');
 
 let activeElement = 0;
+let activeSmallElement = 0;
+let activeTitleElement = 0;
 
 const buttonNext = document.querySelector('div.my-next');
 
 buttonNext.addEventListener('click', function(){
     mainCarouselImages[activeElement].classList.remove('active');
+    smallCarouselImages[activeSmallElement].classList.remove('small-active');
+    typeOfAnimal[activeTitleElement].classList.remove('active-title');
 
     if (activeElement === mainCarouselImages.length - 1){
         activeElement = 0;
     } else {
         activeElement++;
-    }
+    };
+
+    if (activeSmallElement === smallCarouselImages.length - 1){
+        activeSmallElement = 0;
+    } else {
+        activeSmallElement++;
+    };
+
+    if(activeTitleElement === typeOfAnimal.length - 1){
+        activeTitleElement = 0;
+    } else {
+        activeTitleElement++;
+    };
 
     mainCarouselImages[activeElement].classList.add('active');
+    smallCarouselImages[activeSmallElement].classList.add('small-active');
+    typeOfAnimal[activeTitleElement].classList.add('active-title');
 });
 
 const buttonPrevious = document.querySelector('div.my-previous');
 
 buttonPrevious.addEventListener('click', function(){
     mainCarouselImages[activeElement].classList.remove('active');
+    smallCarouselImages[activeSmallElement].classList.remove('small-active');
+    typeOfAnimal[activeTitleElement].classList.remove('active-title');
 
     if (activeElement === 0){
         activeElement = mainCarouselImages.length - 1;
     } else {
         activeElement--;
-    }
+    };
+
+    if (activeSmallElement === 0){
+        activeSmallElement = smallCarouselImages.length - 1;
+    } else {
+        activeSmallElement--;
+    };
+
+    if(activeTitleElement === 0){
+        activeTitleElement = typeOfAnimal.length - 1;
+    } else {
+        activeTitleElement--;
+    };
 
     mainCarouselImages[activeElement].classList.add('active');
-})
+    smallCarouselImages[activeSmallElement].classList.add('small-active');
+    typeOfAnimal[activeTitleElement].classList.add('active-title');
+});
 
+function createImg (array, container, myClass) {
+    for (let i = 0 ; i < animals.length ; i++){
+        const imageOfAnimal = document.createElement('img');
+        imageOfAnimal.src = `img/${animals[i].image}`;
+        array.push(imageOfAnimal);
+    };
+    const carouselContainer = document.querySelector(container);
 
+    for (let i = 0; i < array.length; i++) {
+        carouselContainer.appendChild(array[i]);
+    };
+
+    array[0].classList.add(myClass);
+};
+
+for (let i = 0 ; i < animals.length ; i++){
+    const textContainer = document.createElement('div');
+    textContainer.classList.add('text-container');
+    document.querySelector('.my-carousel-images').appendChild(textContainer);
+    const title = document.createElement('h1');
+    textContainer.appendChild(title);
+    title.innerHTML = animals[i].animal;
+    typeOfAnimal.push(title);
+};
+
+const carouselTitleContainer = document.querySelector('div.text-container');
+
+for (let i = 0; i < typeOfAnimal.length; i++) {
+    carouselTitleContainer.appendChild(typeOfAnimal[i]);
+};
+
+typeOfAnimal[0].classList.add('active-title');
